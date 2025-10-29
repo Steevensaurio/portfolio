d302a504ccad8e4f87a12ac928159f7421445f52
Steeven Maila <steevenandresmaila@gmail.com>
2025-10-29 18:13:44 -0500

    Probando estilos con TailwindCss

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 function App() {
 
   return (
-    <div className='flex justify-center items-center bg-black h-screen'>
+    <div className='flex justify-center items-center bg-black h-dvh'>
       <div className='text-center'>
         <h1 className='text-2xl font-bold text-white'>Hola Mi Amor</h1>
         <p className='text-white'>Te quiero mucho ❤️ </p>
